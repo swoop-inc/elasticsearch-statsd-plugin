0351865b796d1bc2031d1682f147bcc0048c0e5d
Xiao Yu <me@xyu.io>
2014-08-07 09:57:41 -0400

    Use common  class to report shard stats

--- src/main/java/org/elasticsearch/service/statsd/StatsdReporterIndices.java
+++ src/main/java/org/elasticsearch/service/statsd/StatsdReporterIndices.java
@@ -5,16 +5,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.elasticsearch.index.shard.service.IndexShard;
-import org.elasticsearch.index.get.GetStats;
-import org.elasticsearch.index.indexing.IndexingStats;
-import org.elasticsearch.index.merge.MergeStats;
-import org.elasticsearch.index.refresh.RefreshStats;
-import org.elasticsearch.index.search.stats.SearchStats;
-import org.elasticsearch.index.shard.DocsStats;
-import org.elasticsearch.index.store.StoreStats;
-import org.elasticsearch.index.warmer.WarmerStats;
 
-public class StatsdReporterIndices extends StatsdReporter {
+public class StatsdReporterIndices extends StatsdReporterIndexStats {
 
 	private final List<IndexShard> indexShards;
 
@@ -22,113 +14,26 @@ public class StatsdReporterIndices extends StatsdReporter {
 		this.indexShards = indexShards;
 	}
 
-	public void run()
-	{
+	public void run() {
 		try {
 			for (IndexShard indexShard : this.indexShards) {
-				String type = this.buildMetricName("indexes.") + indexShard.shardId().index().name() + ".id." + indexShard.shardId().id();
-				this.sendSearchStats(type + ".search", indexShard.searchStats());
-				this.sendGetStats(type + ".get", indexShard.getStats());
+				String type = this.buildMetricName("index.") + indexShard.shardId().index().name() + ".shard." + indexShard.shardId().id();
 				this.sendDocsStats(type + ".docs", indexShard.docStats());
-				this.sendRefreshStats(type + ".refresh", indexShard.refreshStats());
-				this.sendIndexingStats(type + ".indexing", indexShard.indexingStats("_all"));
-				this.sendMergeStats(type + ".merge", indexShard.mergeStats());
-				this.sendWarmerStats(type + ".warmer", indexShard.warmerStats());
 				this.sendStoreStats(type + ".store", indexShard.storeStats());
+				this.sendIndexingStats(type + ".indexing", indexShard.indexingStats("_all"));
+				this.sendGetStats(type + ".get", indexShard.getStats());
+				this.sendSearchStats(type + ".search", indexShard.searchStats("_all"));
+				this.sendMergeStats(type + ".merges", indexShard.mergeStats());
+				this.sendRefreshStats(type + ".refresh", indexShard.refreshStats());
+				this.sendFlushStats(type + ".flush", indexShard.flushStats());
+				this.sendFilterCacheStats(type + ".filter_cache", indexShard.filterCacheStats());
+				this.sendIdCacheStats(type + ".id_cache", indexShard.idCacheStats());
+				this.sendFielddataCacheStats(type + ".fielddata", indexShard.fieldDataStats("_all"));
+				this.sendCompletionStats(type + ".completion", indexShard.completionStats("_all"));
+				this.sendSegmentsStats(type + ".segments", indexShard.segmentStats());
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			this.logException(e);
 		}
-	}
-
-	private void sendStoreStats(String type, StoreStats storeStats)
-	{
-		this.sendGauge(type, "sizeInBytes", storeStats.sizeInBytes());
-		this.sendGauge(type, "throttleTimeInNanos", storeStats.throttleTime().getNanos());
-	}
-
-	private void sendWarmerStats(String type, WarmerStats warmerStats)
-	{
-		this.sendGauge(type, "current", warmerStats.current());
-		this.sendGauge(type, "total", warmerStats.total());
-		this.sendTime(type, "totalTimeInMillis", warmerStats.totalTimeInMillis());
-	}
-
-	private void sendMergeStats(String type, MergeStats mergeStats)
-	{
-		this.sendGauge(type, "total", mergeStats.getTotal());
-		this.sendTime(type, "totalTimeInMillis", mergeStats.getTotalTimeInMillis());
-		this.sendGauge(type, "totalNumDocs", mergeStats.getTotalNumDocs());
-		this.sendGauge(type, "current", mergeStats.getCurrent());
-		this.sendGauge(type, "currentNumDocs", mergeStats.getCurrentNumDocs());
-		this.sendGauge(type, "currentSizeInBytes", mergeStats.getCurrentSizeInBytes());
-	}
-
-	private void sendSearchStats(String type, SearchStats searchStats)
-	{
-		SearchStats.Stats totalSearchStats = searchStats.getTotal();
-		this.sendSearchStatsStats(type + "._all", totalSearchStats);
-
-		if (searchStats.getGroupStats() != null) {
-			for (Map.Entry<String, SearchStats.Stats> statsEntry : searchStats.getGroupStats().entrySet()) {
-				this.sendSearchStatsStats(type + "." + statsEntry.getKey(), statsEntry.getValue());
-			}
-		}
-	}
-
-	private void sendSearchStatsStats(String group, SearchStats.Stats searchStats)
-	{
-		String type = this.buildMetricName("search.stats.") + group;
-		this.sendCount(type, "queryCount", searchStats.getQueryCount());
-		this.sendCount(type, "queryTimeInMillis", searchStats.getQueryTimeInMillis());
-		this.sendGauge(type, "queryCurrent", searchStats.getQueryCurrent());
-		this.sendCount(type, "fetchCount", searchStats.getFetchCount());
-		this.sendCount(type, "fetchTimeInMillis", searchStats.getFetchTimeInMillis());
-		this.sendGauge(type, "fetchCurrent", searchStats.getFetchCurrent());
-	}
-
-	private void sendRefreshStats(String type, RefreshStats refreshStats)
-	{
-		this.sendCount(type, "total", refreshStats.getTotal());
-		this.sendCount(type, "totalTimeInMillis", refreshStats.getTotalTimeInMillis());
-	}
-
-	private void sendIndexingStats(String type, IndexingStats indexingStats)
-	{
-		IndexingStats.Stats totalStats = indexingStats.getTotal();
-		this.sendStats(type + "._all", totalStats);
-
-		Map<String, IndexingStats.Stats> typeStats = indexingStats.getTypeStats();
-		if (typeStats != null) {
-			for (Map.Entry<String, IndexingStats.Stats> statsEntry : typeStats.entrySet()) {
-				this.sendStats(type + "." + statsEntry.getKey(), statsEntry.getValue());
-			}
-		}
-	}
-
-	private void sendStats(String type, IndexingStats.Stats stats)
-	{
-		this.sendCount(type, "indexCount", stats.getIndexCount());
-		this.sendCount(type, "indexTimeInMillis", stats.getIndexTimeInMillis());
-		this.sendGauge(type, "indexCurrent", stats.getIndexCount());
-		this.sendCount(type, "deleteCount", stats.getDeleteCount());
-		this.sendCount(type, "deleteTimeInMillis", stats.getDeleteTimeInMillis());
-		this.sendGauge(type, "deleteCurrent", stats.getDeleteCurrent());
-	}
-
-	private void sendGetStats(String type, GetStats getStats)
-	{
-		this.sendCount(type, "existsCount", getStats.getExistsCount());
-		this.sendCount(type, "existsTimeInMillis", getStats.getExistsTimeInMillis());
-		this.sendCount(type, "missingCount", getStats.getMissingCount());
-		this.sendCount(type, "missingTimeInMillis", getStats.getMissingTimeInMillis());
-		this.sendGauge(type, "current", getStats.current());
-	}
-
-	private void sendDocsStats(String name, DocsStats docsStats)
-	{
-		this.sendCount(name, "count", docsStats.getCount());
-		this.sendCount(name, "deleted", docsStats.getDeleted());
 	}
 }
